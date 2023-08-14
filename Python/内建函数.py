f698c4cbb3558c09c3ad7835ecce46f08673d54b
zilyan <2395750512@qq.com>
2023-08-14 17:26:35 +0800

    修改提交，增加Linux命令PDF文件

--- Python/内建函数.py
+++ Python/内建函数.py
@@ -15,3 +15,18 @@ def read_text(path):
 #################多重列表推到式
 lines=['车标','电机舱','发动机','格栅','雨刮','大灯','行李箱']
 tages_lines=[[].append(y)  for x in lines for y in x.lower().replace('/','')]
+
+
+##################动态的创建变量名
+###使用globals
+var_name = "temp"
+globals()[var_name] = "Hello, World!"
+print(temp)  # 输出: Hello, World!
+
+####使用local
+var_name = "temp"
+locals()[var_name] = "Hello, World!"
+print(temp)  # 输出: Hello, World!
+
+
+
